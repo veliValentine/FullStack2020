9394297bfb64b45f1f4e1781a20849b42dc14bf4
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-01 16:10:24 +0200

    osa1 1.3-1.5
    Tehtävät:
    1.3
    1.4
    1.5
    Tehty

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -1,66 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (p) => {
-    console.log('Header ', p)
+const Header = (props) => {
+    console.log('Header ', props.course)
     return (
-        <h1>{p.course}</h1>
+        <h1>{props.course.name}</h1>
     )
 }
 
-const Total = (p) => {
-    console.log('Total exercise ', p.oliot)
+const Total = (props) => {
+    console.log('Total exercise ', props.course)
     let summa = 0
-    
-    p.oliot.forEach(v => {
-        summa += v.exercises
+
+    props.course.parts.forEach(part => {
+        summa += part.exercises
     })
     return (
         <p>Number of exercises {summa}</p>
     )
 }
 
-const Content = (p) => {
-    console.log('Content ', p.oliot)
+const Content = (props) => {
+    console.log('Content ', props.course)
     return (
         <div>
-            <Part olio={p.oliot[0]}/>
-            <Part olio={p.oliot[1]}/>
-            <Part olio={p.oliot[2]}/>
+            <Part part={props.course.parts[0]} />
+            <Part part={props.course.parts[1]} />
+            <Part part={props.course.parts[2]} />
         </div>
     )
 }
 
-const Part = (p) => {
-    console.log('Part ', p.olio)
+const Part = (props) => {
+    console.log('Part ', props.part)
     return (
         <p>
-            {p.olio.name} {p.olio.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     )
 }
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
-
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
     return (
         <div>
             <Header course={course} />
-            <Content oliot={parts} />
-            <Total oliot={parts} />
+            <Content course={course} />
+            <Total course={course} />
         </div>
     )
 }
