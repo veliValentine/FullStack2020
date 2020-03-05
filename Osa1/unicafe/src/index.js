819e1e549626fd6585561c9250c33060e1ad3fe3
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-05 22:41:57 +0200

    osa1 Unicafe 1.7
    Tehtävä 1.6-1.7

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({onClick, text}) => (
-<button onClick={onClick}>{text}</button>
-)
+const Button = ({ onClick, text }) => {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+}
 
 const Title = ({ title }) => <h1>{title}</h1>
 
-const Display = ({ count, name }) => <p>{name} {count}</p>
+const Display = ({ count, name }) => {
+    if (name === 'positive') {
+        return (
+            <p>{name} {count*100}%</p>
+        )
+    }
+    return (
+        <p>{name} {count}</p>)
+}
+
+const Statistics = (props) => {
+
+}
 
 const App = () => {
     // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+    const [sum, setSum] = useState(0)
 
-    const handleGood = () => setGood(good + 1)
-    const handleNeutral = () => setNeutral(neutral + 1)
-    const handleBad = () => setBad(bad + 1)
+    const handleGood = () => {
+        setGood(good + 1)
+        setAll(all + 1)
+        setSum(sum + 1)
+    }
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+        setSum(sum + 0)
+    }
+    const handleBad = () => {
+        setBad(bad + 1)
+        setAll(all + 1)
+        setSum(sum - 1)
+    }
 
     return (
         <div>
@@ -30,6 +58,9 @@ const App = () => {
             <Display count={good} name='good' />
             <Display count={neutral} name='neutral' />
             <Display count={bad} name='bad' />
+            <Display count={all} name='all' />
+            <Display count={sum / all} name='average' />
+            <Display count={good / all} name='positive' />
         </div>
     )
 }
