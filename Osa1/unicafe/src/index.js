ffa7de1288455ef5c1daf7a642623b6d441d98b9
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-05 22:21:50 +0200

    osa1 unicafe 1.6
    tehtävä 1.6

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Button = ({onClick, text}) => (
+<button onClick={onClick}>{text}</button>
+)
 
-  return (
-    <div>
-      code here
-    </div>
-  )
+const Title = ({ title }) => <h1>{title}</h1>
+
+const Display = ({ count, name }) => <p>{name} {count}</p>
+
+const App = () => {
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGood = () => setGood(good + 1)
+    const handleNeutral = () => setNeutral(neutral + 1)
+    const handleBad = () => setBad(bad + 1)
+
+    return (
+        <div>
+            <Title title={'give feedback'} />
+            <Button onClick={handleGood} text='good' />
+            <Button onClick={handleNeutral} text='neutral' />
+            <Button onClick={handleBad} text='bad' />
+
+            <Title title={'statistics'} />
+            <Display count={good} name='good' />
+            <Display count={neutral} name='neutral' />
+            <Display count={bad} name='bad' />
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
+ReactDOM.render(<App />,
+    document.getElementById('root')
 )
