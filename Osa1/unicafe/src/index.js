50f3a7c1089b3f16f601fbd9b002251090c01fe3
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-05 22:54:40 +0200

    osa1 unicafe 1.8

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -10,17 +10,27 @@ const Button = ({ onClick, text }) => {
 const Title = ({ title }) => <h1>{title}</h1>
 
 const Display = ({ count, name }) => {
-    if (name === 'positive') {
-        return (
-            <p>{name} {count*100}%</p>
-        )
-    }
     return (
         <p>{name} {count}</p>)
 }
 
-const Statistics = (props) => {
+const Statistics = ({good, neutral, bad}) => {
+    const all = good + bad + neutral
+    const sum = good - bad
+    const average = sum / all
+    const positive = good / all
 
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
+        </div>
+    )
 }
 
 const App = () => {
@@ -28,23 +38,15 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [all, setAll] = useState(0)
-    const [sum, setSum] = useState(0)
 
     const handleGood = () => {
         setGood(good + 1)
-        setAll(all + 1)
-        setSum(sum + 1)
     }
     const handleNeutral = () => {
         setNeutral(neutral + 1)
-        setAll(all + 1)
-        setSum(sum + 0)
     }
     const handleBad = () => {
         setBad(bad + 1)
-        setAll(all + 1)
-        setSum(sum - 1)
     }
 
     return (
@@ -54,13 +56,7 @@ const App = () => {
             <Button onClick={handleNeutral} text='neutral' />
             <Button onClick={handleBad} text='bad' />
 
-            <Title title={'statistics'} />
-            <Display count={good} name='good' />
-            <Display count={neutral} name='neutral' />
-            <Display count={bad} name='bad' />
-            <Display count={all} name='all' />
-            <Display count={sum / all} name='average' />
-            <Display count={good / all} name='positive' />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
