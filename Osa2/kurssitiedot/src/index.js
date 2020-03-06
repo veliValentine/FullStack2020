ec7c230d9aa6ad930e94255c9c2f9deeb6e7d82c
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-06 17:52:10 +0200

    osa2 kurssitiedot index.js update

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Course = (props) => {
+    console.log('Header ', props.course)
+    return (
+        <h1>{props.course.name}</h1>
+    )
+}
+ 
+const Total = (props) => {
+    console.log('Total exercise ', props.course)
+    let summa = 0
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    props.course.parts.forEach(part => {
+        summa += part.exercises
+    })
+    return (
+        <p>Number of exercises {summa}</p>
+    )
+}
+
+const Content = (props) => {
+    console.log('Content ', props.course)
+    return (
+        <div>
+            <Part part={props.course.parts[0]} />
+            <Part part={props.course.parts[1]} />
+            <Part part={props.course.parts[2]} />
+        </div>
+    )
+}
+
+const Part = (props) => {
+    console.log('Part ', props.part)
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
