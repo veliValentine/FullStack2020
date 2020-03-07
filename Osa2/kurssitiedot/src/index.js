7d9a4ad7dfacdbaf60870be6af3672db240ab8ae
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-07 11:19:36 +0200

    osa2 Kurssitiedot 2.1

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -2,72 +2,95 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Course = (props) => {
-    console.log('Header ', props.course)
-    return (
-        <h1>{props.course.name}</h1>
-    )
-}
- 
-const Total = (props) => {
-    console.log('Total exercise ', props.course)
-    let summa = 0
+  console.log('Course', {props});
+  
+  const course = props.course
+  console.log({course});
 
-    props.course.parts.forEach(part => {
-        summa += part.exercises
-    })
-    return (
-        <p>Number of exercises {summa}</p>
-    )
+  const name = course.name
+  console.log({name});
+
+  const id = course.id
+  console.log({ id });
+
+  const parts = course.parts
+  console.log({ parts });
+
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log('Header',{props});
+  
+  const name = props.name
+  console.log({name});
+  
+  return (
+    <h1>{name}</h1>
+  )
 }
 
 const Content = (props) => {
-    console.log('Content ', props.course)
-    return (
-        <div>
-            <Part part={props.course.parts[0]} />
-            <Part part={props.course.parts[1]} />
-            <Part part={props.course.parts[2]} />
-        </div>
-    )
+  console.log('Content', {props});
+  
+  const parts = props.parts
+  console.log({parts});
+
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
 }
 
 const Part = (props) => {
-    console.log('Part ', props.part)
-    return (
-        <p>
-            {props.part.name} {props.part.exercises}
-        </p>
-    )
+  console.log('Part',{props})
+  
+  const part = props.part
+  console.log({part});
+
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
 }
 
 const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        }
-      ]
-    }
-  
-    return (
-      <div>
-        <Course course={course} />
-      </div>
-    )
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
+
+  console.log('const Course', {course});
+  
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
