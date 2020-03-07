4efb19eba67876ba22ff6db9f0dae7a5642e3257
veliValentine <nicolas.valentine@helsinki.fi>
2020-03-07 15:03:08 +0200

    osa2 Kurssitiedot 2.4
    Tehty tehtävä 2.4

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Courses = (props) => {
+  console.log('Courses', { props });
+
+  const courses = props.courses
+  console.log({ courses });
+
+  return (
+    <div>
+      <h2>Web development curriculum</h2>
+      {courses.map(course => <Course key={course.id} course={course} />
+      )}
+    </div>
+  )
+
+}
+
 const Course = (props) => {
   console.log('Course', { props });
 
@@ -31,7 +47,7 @@ const Header = (props) => {
   console.log({ name });
 
   return (
-    <h1>{name}</h1>
+    <h3>{name}</h3>
   )
 }
 
@@ -85,38 +101,58 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  console.log('App');
 
-  console.log('const Course', { course });
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  console.log('const Course', { courses });
 
   return (
     <div>
-      <Course course={course} />
+      <Courses courses={courses} />
     </div>
   )
 }
