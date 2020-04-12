8e9ea815654444934a7f457df3345aba34802633
veliValentine <nicolas.valentine@helsinki.fi>
2020-04-12 22:49:35 +0300

    osa1 anekdootit 1.11-1.14
    osa1 Anekdootit lopullinen versio

--- Osa1/anekdootit/src/index.js
+++ Osa1/anekdootit/src/index.js
@@ -1,12 +1,85 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = ({ text }) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Anecdote = ({ anecdote, votes }) => {
+  return (
+    <div>
+      <Text text={anecdote} />
+      <Votes votes={votes} />
+    </div>
+  )
+}
+
+const Votes = ({ votes }) => {
+  return (
+    <div>
+      has {votes} votes
+    </div>
+  )
+}
+
+const Text = ({ text }) => {
+  return (
+    <div>
+      {text}
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0))
+  const [mostVotes, setMostVotes] = useState(selected)
+
+  const handleNextClick = () => {
+    let randomNumber = getRandomInt(anecdotes.length)
+    if (randomNumber === selected) {
+      randomNumber -= 1
+      if (randomNumber < 0) {
+        randomNumber += 2
+      }
+    }
+    console.log(`CLICK! Random number:${randomNumber}`)
+    setSelected(randomNumber)
+  }
+
+  const handleVote = () => {
+    console.log(`voted:${selected}`)
+    const copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
+
+    if (votes[selected] >= votes[mostVotes]) {
+      setMostVotes(selected)
+    }
+  }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <Title text={'Anecdote of the day'} />
+      <Anecdote anecdote={anecdotes[selected]} votes={votes[selected]} />
+      <Button onClick={handleVote} text={'vote'} />
+      <Button onClick={handleNextClick} text={'next anecode'} />
+      <Title text={'Anecdote with most votes'} />
+      <Anecdote anecdote={anecdotes[mostVotes]} votes={votes[mostVotes]} />
     </div>
   )
 }
