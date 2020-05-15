c85e269f3437e661a5824d847165332b41027ab9
veliValentine <nicolas.valentine@helsinki.fi>
2020-05-15 10:22:13 +0300

    osa4 blogilista 4.5

--- Osa4/Blogilista/utils/list_helper.js
+++ Osa4/Blogilista/utils/list_helper.js
@@ -5,12 +5,23 @@ const dummy = () => {
 const totalLikes = (blogs) => {
   let totalLikes = blogs.reduce((sum, blog) => {
     return sum += blog.likes
-  },0)
+  }, 0)
 
   return totalLikes
+}
+
+const favoriteBlog = (blogs) => {
+  let blogWithMostLikes
+  blogs.forEach(blog => {
+    blogWithMostLikes = blogWithMostLikes || blog
+    blogWithMostLikes = blog.likes > blogWithMostLikes.likes ? blog : blogWithMostLikes
+  })
+
+  return blogWithMostLikes === undefined ? {} : blogWithMostLikes
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
