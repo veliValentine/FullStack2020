9368025660269781d585eca469f02aed9afa1090
veliValentine <nicolas.valentine@helsinki.fi>
2020-05-15 09:53:06 +0300

    osa4 blogilista 4.4

--- Osa4/Blogilista/utils/list_helper.js
+++ Osa4/Blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let totalLikes = blogs.reduce((sum, blog) => {
+    return sum += blog.likes
+  },0)
+
+  return totalLikes
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
