34bbc219ee5b1e6febb8b9ee2b84e138717d0a2b
veliValentine <nicolas.valentine@helsinki.fi>
2020-05-19 22:15:54 +0300

    osa4 blogilista 4.18 & 4.19

--- Osa4/Blogilista/controllers/blogs.js
+++ Osa4/Blogilista/controllers/blogs.js
@@ -1,17 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-    .find({}).populate('user', { username: 1, name: 1, id: 1 })
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
 
   response.json(blogs.map(b => b.toJSON()))
 })
 
 blogsRouter.get('/:id', async (request, response) => {
   const blog = await Blog
-    .findById(request.params.id).populate('user', { username: 1, name: 1, id: 1 })
+    .findById(request.params.id)
+    .populate('user', { username: 1, name: 1, id: 1 })
+
   if (blog) {
     response.json(blog.toJSON())
   } else {
@@ -21,8 +33,15 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -35,6 +54,7 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog.id)
   await user.save()
+
   response.json(savedBlog.toJSON())
 })
 
--- Osa4/Blogilista/utils/middleware.js
+++ Osa4/Blogilista/utils/middleware.js
@@ -25,7 +25,7 @@ const errorHandler = (error, request, response, next) => {
     })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({
-      error: ' invalid token'
+      error: 'invalid token'
     })
   }
 
