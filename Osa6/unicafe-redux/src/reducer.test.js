8fcca5e9fc512d7ff8f68f8ee41e57e3af4fbac4
veliValentine <nicolas.valentine@helsinki.fi>
2020-06-02 20:33:17 +0300

    osa6 unicafe 6.1

--- Osa6/unicafe-redux/src/reducer.test.js
+++ Osa6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,71 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('good is incremented twice', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(
+      counterReducer(state, action), action
+    )
+    expect(newState).toEqual({
+      good: 2,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('neutral is increment', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is increment', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero zeroes values', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    const state = {
+      good: 1,
+      ok: 1,
+      bad: 1
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
