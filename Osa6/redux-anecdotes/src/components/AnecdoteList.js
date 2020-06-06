b4b2bda8a035d409991781ab6bd8b2106495aa31
veliValentine <nicolas.valentine@helsinki.fi>
2020-06-06 13:28:13 +0300

    osa6 anekdootti 6.12

--- Osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -19,7 +19,12 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
+  const filter = useSelector(state => state.filter)
   const anecdotes = useSelector(state => state.anecdotes)
+    .filter(a =>
+      a.content.toLowerCase()
+        .includes(filter.toLowerCase())
+    )
 
   const sortByLikes = (a1, a2) => a2.votes - a1.votes
 
