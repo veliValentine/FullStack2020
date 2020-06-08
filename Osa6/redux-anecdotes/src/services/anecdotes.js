889ec602f7e7f152d096a73ed8f8e1f614dbb2e9
veliValentine <nicolas.valentine@helsinki.fi>
2020-06-08 18:28:41 +0300

    osa6 anecdotes 6.13

---
+++ Osa6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+  const response = await axios.get(baseURL)
+  return response.data
+}
+
+export default {
+  getAll,
+}
