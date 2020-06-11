c3c7f6dfe027772933cf20ee1fe1fd0e75f246a0
veliValentine <nicolas.valentine@helsinki.fi>
2020-06-11 22:27:32 +0300

    osa7 7.5

--- Osa7/anecdotes/src/App.js
+++ Osa7/anecdotes/src/App.js
@@ -83,6 +83,12 @@ const CreateNew = (props) => {
     history.push('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -99,7 +105,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="reset" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
