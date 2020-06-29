cf72a2907dae7ea2c8525b50a6d4deb22ee51b44
veliValentine <nicolas.valentine@helsinki.fi>
2020-06-29 05:59:17 +0300

    osa8 8.13

--- Osa8/books/index.js
+++ Osa8/books/index.js
@@ -1,5 +1,20 @@
+require('dotenv').config()
 const { ApolloServer, gql } = require('apollo-server')
 const { v4: uuid } = require('uuid')
+const mongoose = require('mongoose')
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 let authors = [
   {
@@ -88,9 +103,9 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
+    author: Author!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
@@ -118,23 +133,14 @@ const typeDefs = gql`
   }
 `
 
-const filterBy = (a, b) => a === b
-
 const resolvers = {
   Query: {
     allBooks: (root, { author, genre }) => {
-      let returnedBooks = [...books]
-      if (author) {
-        returnedBooks = returnedBooks.filter(b => b.author === author)
-      }
-      if (genre) {
-        returnedBooks = returnedBooks.filter(b => b.genres.includes(genre))
-      }
-      return returnedBooks
+      return Book.find({})
     },
-    bookCount: () => books.length,
-    allAuthors: () => authors,
-    authorCount: () => authors.length,
+    bookCount: () => Book.collection.countDocuments(),
+    allAuthors: () => Author.find({}),
+    authorCount: () => Author.collection.countDocuments(),
   },
   Author: {
     bookCount: ({ name }) => books.filter(a => a.author === name).length
