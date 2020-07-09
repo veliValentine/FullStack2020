7a0005d666dd05e7b5a6961d5403ba0e987ac3a2
veliValentine <nicolas.valentine@helsinki.fi>
2020-07-09 08:16:41 +0300

    part9 9.14 works again

--- Osa9/courses/src/index.tsx
+++ Osa9/courses/src/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App: React.FC = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const totalExerciseCount = () => courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)
+
+  return (
+    <div>
+      <Header header={courseName} />
+      <Content courses={courseParts} />
+      <Total total={totalExerciseCount()} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
