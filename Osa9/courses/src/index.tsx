b3cd2dd0e75445aefdd8365994954fad198d27eb
veliValentine <nicolas.valentine@helsinki.fi>
2020-07-09 11:22:18 +0300

    1. task of 9.15

--- Osa9/courses/src/index.tsx
+++ Osa9/courses/src/index.tsx
@@ -4,22 +4,52 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Total from "./components/Total";
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartBaseDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartBaseDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBaseDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
+
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is an awesome course part"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+  }
+];
+
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
 
   const totalExerciseCount = () => courseParts.reduce((carry, part) => carry + part.exerciseCount, 0);
 
